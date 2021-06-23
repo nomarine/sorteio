c41f3ee2bb263b22bd088bb466b4f9989c0338a7
nomarine <rafaelfijos@gmail.com>
2021-06-23 16:35:51 -0300

    Tela de consulta de quem o participante tirou no sorteio

--- mostrarSorteado.php
+++ mostrarSorteado.php
@@ -1,11 +1,16 @@
 <?php
     require('conexao_db.php');
+    include('debugging.php');
 
     $codigo = $_GET['codigo'];
 
-    $sql_select = "SELECT nome, codigo FROM participantes WHERE codigo = '" . $codigo . "'";
+    //$sql_select = "SELECT participantes.nome, participantes.codigo, sorteados.codigo FROM participantes, sorteados WHERE participantes.sorteado_id = sorteados.id = '" . $codigo . "';";
+    $sql_select = "SELECT participantes.nome, participantes.codigo, participantes.sorteado_id, sorteados.nome AS sorteado FROM participantes, sorteados WHERE participantes.codigo = '" . $codigo . "';";
     $resultado = $conexao->query($sql_select);
     $participante = $resultado->fetch_assoc();
+    console_log($participante);
 
-    echo "<p>" .  $participante["nome"] . " - Você tirou " . ucwords($participante["nome"]) . "</p>";
+    echo "<p>" .  ucwords($participante["nome"]) . " você tirou " . ucwords($participante["sorteado"]) . "</p>";
+
+    
 ?>
--- sortear.php
+++ sortear.php
@@ -2,19 +2,52 @@
     include('debugging.php');
     require('conexao_db.php');
 
+    //Criar lista de nomes
+    $sorteados = [];
+    $index = 0;
+    $sql_select = "SELECT * FROM participantes";
+    $resultado = $conexao->query($sql_select);
+    while($registro = $resultado->fetch_assoc()){
+        $sorteados[$index] = $registro["nome"];
+        $index++;
+    }
+    
+    shuffle($sorteados);
+
+    //Tratar último valor igual
+    $sql_select = "SELECT * FROM participantes ORDER BY ID DESC LIMIT 1";
+    $resultado = $conexao->query($sql_select);
+    $ultimoregistro = mysqli_fetch_array($resultado);
+    if(end($sorteados) == $ultimoregistro["nome"]){
+        $ultimoindex = count($sorteados) - 1;
+        $ultimonome = $sorteados[$ultimoindex];
+        $sorteados[$ultimoindex] = $sorteados[$ultimoindex-1];
+        $sorteados[$ultimoindex-1] = $ultimonome;
+    }
+    
+    //Tratar valores iguais
+    foreach($sorteados as $index => $participante){
+        if($index == count($sorteados)-1){
+            break;
+        }
+        if($participante == $sorteados[$index]){
+            $proximonome = $sorteados[$index];
+            $sorteados[$index] = $sorteados[$index+1]; 
+            $sorteados[$index+1] = $proximonome;
+        }
+    } 
+    
+    //Vincular sorteados à matriz de participantes
     $sql_select = "SELECT * FROM sorteados";
     $resultado = $conexao->query($sql_select);
     $registro = $resultado->fetch_assoc();
-    
+    console_log($registro);
     if(empty($registro['id'])){
         console_log('Tabela vazia');
-        $sql_select = "SELECT nome FROM participantes ORDER BY RAND();";
-        $resultado = $conexao->query($sql_select);
         $sql_insert = "";
-
         //Atribui a cada registro um código e popula os sorteados com os nomes dos participantes;
-        while($linha = $resultado->fetch_assoc()) {
-            $sql_insert .= "INSERT INTO sorteados (nome) VALUES ('" . $linha["nome"] . "'); ";
+        foreach($sorteados as $index => $participante){
+            $sql_insert .= "INSERT INTO sorteados (nome) VALUES ('" . $participante . "');";
         }
 
         if($conexao->multi_query($sql_insert) === TRUE) {
@@ -22,39 +55,21 @@
         } else {
             echo "Error<br>" . $sql_insert . "<br>" . $conexao->error;
         }
+
     } else {
         console_log('Tabela com conteúdo');
     }
-    
-/*     foreach($_SESSION["participantes"] as $index => $participante){
-        $_SESSION["participantes"][$index]["codigo"] = gerarCodigo($caracteres, 5);
-        $_SESSION["sorteados"][$index]=$_SESSION["participantes"][$index]["nome"];
+
+    $sql_update = "";
+    foreach($sorteados as $index => $nome){
+        $sql_update .= "UPDATE participantes SET sorteado_id='" . $index+1 . "' WHERE id='" . $index+1 . "';";
+        $index++;
     }
-    shuffle($_SESSION["sorteados"]);
-    
-    //Tratar último valor igual
-    $coluna = array_column($_SESSION["participantes"],0);
-    if(end($coluna) == end($_SESSION["sorteados"])){
-        $lastindex = count($_SESSION["sorteados"]) - 1;
-        $ultimonome = $_SESSION["sorteados"][$lastindex];
-        $_SESSION["sorteados"][$lastindex] = $_SESSION["sorteados"][$lastindex-1];
-        $_SESSION["sorteados"][$lastindex-1] = $ultimonome;
+    if($conexao->multi_query($sql_update) === TRUE) {
+        console_log('Tabela participantes atualizada!');
+    } else {
+        console_log($conexao->error);
     }
-    //Tratar valores iguais
-    foreach($_SESSION["participantes"] as $index => $participante){
-        if($index == count($_SESSION["participantes"])-1){
-            break;
-        }
-        if($participante["nome"] == $_SESSION["sorteados"][$index]){
-            $proximonome = $_SESSION["sorteados"][$index];
-            $_SESSION["sorteados"][$index] = $_SESSION["sorteados"][$index+1]; 
-            $_SESSION["sorteados"][$index+1] = $proximonome;
-        }
-    } 
-    
-    //Vincular sorteados à matriz de participantes
-    for($i=0;$i<count($_SESSION["participantes"]);$i++) {
-        $_SESSION["participantes"][$i]["sorteado"] = $_SESSION["sorteados"][$i];
-    } */
+
     $conexao->close();
 ?>
